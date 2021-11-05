772b5536e1af613718c08986ba00259da925e08b
pydevnepal <rajukhatri054@gmail.com>
2021-11-05 09:31:24 +0545

    set my account and initilizer acc

--- programs/anchor-project/src/lib.rs
+++ programs/anchor-project/src/lib.rs
@@ -1,14 +1,34 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("FEhCr41qTvvkS7yK62RBKPvg7tzhtgu4k9i7buNJ9j8X");
 
 #[program]
 pub mod anchor_project {
     use super::*;
     pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+        let my_ac = &mut ctx.accounts.my_account;
+        my_ac.authority = *ctx.accounts.authority.key;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(
+        init,
+        payer = authority,
+        space = 8 //account discriminator
+        + 32 //pubkey
+        + 566 //make the post max 566 bytes
+    )]
+    pub my_account: Account<'info, MyAccount>,
+    // #[account()]
+    pub authority: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct MyAccount {
+    pub latest_post: u64,
+    pub authority: Pubkey,
+}
